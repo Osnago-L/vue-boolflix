fe9429bea29b0fd3f9604ace239b1cbc44e010c6
Osnago-L <osna.brera@gmail.com>
2022-02-03 13:52:15 +0100

    milestone 2 - stars (milestone3)

--- src/components/commons/MainCard.vue
+++ src/components/commons/MainCard.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="main-card">
     <div>
-      <span class="title">{{dataObj.title}}</span>
-      <span class="original_title">{{dataObj.original_title}}</span>
-      <span class="language">{{dataObj.original_language}}</span>
-      <span class="vote">{{dataObj.vote_average}}</span>
+      <div class="titles">
+        <span class="title">{{switchCategory()}}</span>
+        <span v-if="dataObj.original_title!=undefined" class="original_title">{{dataObj.original_title}}</span>
+      </div>
+      <div class="lang-vote">
+        <div>        
+          <span class="language">{{dataObj.original_language}}</span>
+          <img v-if="dataObj.origin_country != undefined" :src="getFlags()" alt="">
+        </div>    
+        <div><span class="vote" v-for="element,index in roundVote()" :key="index">*</span></div>
+      </div>
     </div>
   </div>
 </template>
@@ -14,6 +21,31 @@ export default {
     name: "MainCard",
     props:{
       dataObj:Object
+    },
+    methods:{
+      getFlags: function(){
+       let country = this.dataObj.origin_country[0]
+       return "https://flagcdn.com/w20/"+ country.toLowerCase() +".png"
+      //  == "en" ? "https://flagcdn.com/w20/us.png" : "https://flagcdn.com/w20/"+ this.dataObj.original_language +".png"
+      },
+      switchCategory: function(){
+        if(this.dataObj.title == undefined){
+          return  this.dataObj.name
+        }else{
+          return this.dataObj.title
+        }
+      },
+      roundVote:function(){
+        return Math.round(this.dataObj.vote_average/2)
+      }
+    },
+    created:function(){
+       this.switchCategory()
+    },
+    watch:{
+      dataObj: function(){
+        this.switchCategory()
+      }
     }
 }
 </script>
@@ -21,17 +53,20 @@ export default {
 <style lang="scss" scoped>
 .main-card{
 
-  div{
+  &>div{
     width: 200px;
     height: 200px;
-    margin: 10px 10px;
     border: 1px solid black;
     padding: 10px;
 
-    display: grid;
-    grid-template-columns: 1fr;
-    grid-template-rows: repeat(4,1fr);
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
   }
+  .titles{
+    display: flex;
+    flex-direction: column;
+
   .title{
     font-weight: bold;
   }
@@ -40,11 +75,27 @@ export default {
     font-size: 13px;
   }
 
+  }
+
+  .lang-vote{
+    display: flex;
+    justify-content: space-between;
+  }
+  
   .language{
-    
+    padding: 3px;
+    margin-right: 5px;
+    background-color: black;
+    font-weight: bold;
+    color: white;
     text-transform: uppercase;
+    border-radius: 5px 5px;
 
   }
+    img{
+    border: 0.5px solid rgba(0, 0, 0, 0.15);
+  }
+
   .vote{
     font-weight: bold;
   }
